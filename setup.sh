a8ae92717dab5a74f035147e07f00db16bbb6ad8
Naman Saraswat <68191729+namansaraswat0406@users.noreply.github.com>
2024-11-21 19:09:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # Store workspace public IP to config file
-IP_VAR="$(curl http://checkip.amazonaws.com/)"
+IP_VAR="$(curlhttps://qkart-backend-hw45.onrender.com/)"
 CONFIG='{"workspaceIp": "'"$IP_VAR"'"}'
 echo $CONFIG > src/ipConfig.json
